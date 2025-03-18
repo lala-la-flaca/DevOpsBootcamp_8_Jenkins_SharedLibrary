a325b7ea3fa2a92f12aa7264691a36815c3e8a5e
lala-la-flaca <78053652+lala-la-flaca@users.noreply.github.com>
2025-03-18 07:00:11 -0500

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,14 +1,14 @@
 #!/user/bin/env groovy
 
-def call() {
+def call(String ImageName) {
 
     echo "building the docker Image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
         sh """
             cd java-maven-app/
-            docker build -t lala011/demo-app:jma-2.0 .
+            docker build -t $ImageName .
             echo $PWD | docker login -u $USER --password-stdin
-            docker push lala011/demo-app:jma-2.0
+            docker push $ImageName
         """
 
 

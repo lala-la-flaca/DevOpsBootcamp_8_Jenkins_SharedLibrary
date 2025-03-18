471703a9819dd74ccbcdcdfcd662e7bd9df4f1a9
lala-la-flaca <78053652+lala-la-flaca@users.noreply.github.com>
2025-03-18 07:40:51 -0500

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,16 +1,20 @@
 #!/user/bin/env groovy
 
+import com.example.Docker
+
 def call(String ImageName) {
 
-    echo "building the docker Image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
-        sh """
-            cd java-maven-app/
-            docker build -t $ImageName .
-            echo $PWD | docker login -u $USER --password-stdin
-            docker push $ImageName
-        """
+    return new Docker(this).buildDockerImage(imageName)
+
+    //echo "building the docker Image..."
+   // withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
+      //  sh """
+       //     cd java-maven-app/
+        //    docker build -t $ImageName .
+        //    echo $PWD | docker login -u $USER --password-stdin
+       //    docker push $ImageName
+       // """
 
 
-    }
+    //}
 }

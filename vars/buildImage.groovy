81e4c8d728780cdd005618ea85d807f9f1dc26b0
Unicorn <lala.la.flaca11@gmail.com>
2025-03-20 09:00:21 -0500

    adding

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -6,15 +6,16 @@ def call(String imageName) {
     return new Docker(this).buildDockerImage(imageName)
 }
 
-    //echo "building the docker Image..."
-   // withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
-      //  sh """
-       //     cd java-maven-app/
-        //    docker build -t $ImageName .
-        //    echo $PWD | docker login -u $USER --password-stdin
-       //    docker push $ImageName
-       // """
 
 
-    //}
+       // echo "building the docker Image..."
+        //withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
+       // sh """
+       //          cd java-maven-app/
+       //          docker build -t $ImageName .
+        //         echo $PWD | docker login -u $USER} --password-stdin
+         //        docker push $ImageName
+         //   """
+       // }
+
 //}
